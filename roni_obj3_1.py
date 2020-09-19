218e3953db7bdadc1de431697a66bea7c1940bcc
Roni Mukherjee <mukherjeeroni97@gmail.com>
2020-09-18 19:03:58 -0600

    Update roni_obj3_1.py

--- roni_obj3_1.py
+++ roni_obj3_1.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 	
 
 	proc1 = os.popen('sudo -S ovs-vsctl set-controller br0 tcp:10.20.30.2:6633')
-
+	time.sleep(10)
 	proc2=subprocess.Popen(["sudo -S ovs-vsctl show"],shell=True,stdout=subprocess.PIPE)
 	temp=proc2.communicate()[0]
 	decoded=temp.decode("ascii")
@@ -21,9 +21,9 @@ if __name__ == "__main__":
 	reg2=r'(?m)(?<=\bfail_mode: ).*$'
 	q2=re.findall(reg2,decoded)
 	
-	if q1=="true":
-		print("The OpenFlow connection between OVS and controller is established.")
+	if q1[0]=="true":
+		print("****** OpenFlow channel connected!")
 	else:
-		print("The OpenFlow connection between OVS and controller is not established.")
+		print("****** OpenFlow channel not connected!")
 	
-	print("The OVS is currently in {} mode.".format(q2[0]))
+	print("Open vSwitch failover mode: {}".format(q2[0]))
