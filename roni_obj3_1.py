3057aee96bbf650d32cbb466abbb3f72bbb198cd
Roni Mukherjee <mukherjeeroni97@gmail.com>
2020-09-18 18:54:47 -0600

    Update roni_obj3_1.py

--- roni_obj3_1.py
+++ roni_obj3_1.py
@@ -8,33 +8,22 @@ import time
 
 if __name__ == "__main__":
 	
-	#commands for the configuration or bridge, port and controller
-	commands=[
-	"echo sdn | sudo -S ovs-vsctl add-br P_bridge_MGMT_G",
-	"echo sdn | sudo -S ovs-vsctl add-port P_bridge_MGMT_G eno1",
-	"echo sdn | sudo -S ovs-vsctl set-controller P_bridge_G tcp:10.20.30.2:6633"
-	]
 
-	b = os.popen('sudo -S ovs-vsctl set-controller P_bridge_G tcp:10.20.30.2:6633')
-	#subprocess to execute commands on linux server
-	k=subprocess.Popen(commands,shell=True,stdout=subprocess.PIPE)
-	
-	time.sleep(30)
-	
-	#verifying the OpenFlow channel between switch and controller
-	x=subprocess.Popen(["echo sdn | sudo -S ovs-vsctl show"],shell=True,stdout=subprocess.PIPE)
-	y=x.communicate()[0]
-	z=y.decode("ascii")
+	proc1 = os.popen('sudo -S ovs-vsctl set-controller br0 tcp:10.20.30.2:6633')
+
+	proc2=subprocess.Popen(["sudo -S ovs-vsctl show"],shell=True,stdout=subprocess.PIPE)
+	temp=proc2.communicate()[0]
+	decoded=temp.decode("ascii")
 	
 	reg1=r'(?m)(?<=\bis_connected: ).*$'
-	find1=re.findall(reg1,z)
+	q1=re.findall(reg1,decoded)
 	
 	reg2=r'(?m)(?<=\bfail_mode: ).*$'
-	find2=re.findall(reg2,z)
+	q2=re.findall(reg2,decoded)
 	
-	if find1=="true":
+	if q1=="true":
 		print("The OpenFlow connection between OVS and controller is established.")
 	else:
 		print("The OpenFlow connection between OVS and controller is not established.")
 	
-	print("The OVS is currently in {} mode.".format(find2[0]))
+	print("The OVS is currently in {} mode.".format(q2[0]))
